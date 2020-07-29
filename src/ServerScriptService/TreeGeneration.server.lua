d5a80ab88ea9ab75307132a2e8daa4639ba19c9c
Chaotic-Cody <codymessinacs@gmail.com>
2020-07-29 00:07:59 -0400

    remove seed

--- src/ServerScriptService/TreeGeneration.server.lua
+++ src/ServerScriptService/TreeGeneration.server.lua
@@ -2,7 +2,6 @@ local Tree = require(game:GetService("ServerStorage"):WaitForChild("Tree"))
 
 local position = Vector3.new(0, 0, 0)
 local size = Vector3.new(25, 50, 25)
-local seed = math.random(-10000, 10000)
 local leafColor = Color3.fromRGB(25, 63, 0)
 local trunkColor = Color3.fromRGB(56, 30, 0)
 
@@ -11,5 +10,5 @@ local parent = Workspace
 --[[
     Testing standard tree placement abd drawing
 ]]
-local christmasTree = Tree.new(position, size, seed, leafColor, trunkColor)
-christmasTree:Draw(seed, parent)
+local christmasTree = Tree.new(position, size, leafColor, trunkColor)
+christmasTree:Draw(parent)
